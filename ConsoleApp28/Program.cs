dd2b8e2dfc4add583fe0fc5139562f7e46e08e0c
user10 <work20go@mail.ru>
2022-11-19 12:00:21 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,28 @@
+﻿//Вариант 20
+int mainDiagonal, allNumSum = 0;
+int mainDiagonalNum = 0;
+int assigment(int[,] sub)
+{
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i != j) sum += sub[i,j];
+        }
+    }
+    return sum;
+}
+int[,] mas = new int[5,5];
+Random r = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+        mas[i, j] = r.Next(20, 40);
+        Console.Write(mas[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine(assigment(mas));
